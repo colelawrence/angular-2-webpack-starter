c76ca1712d5e890d42173a1a776edece16c4c50a
Cole R Lawrence <Colelawr@gmail.com>
2016-05-25 17:50:36 -0500

    Tutorial Step 3: Master/Detail completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,11 @@ import '../../public/css/styles.css'
 })
 
 export class AppComponent {
+  public heroes = HEROES
   title = 'Tour of Heroes'
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  selectedHero: Hero
+  onSelect(hero: Hero) {
+    this.selectedHero = hero
   }
 }
 
@@ -20,3 +21,16 @@ export class Hero {
   id: number
   name: string
 }
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+]
